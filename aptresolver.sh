be6021d4a46713867bdfc2b5d90ab3eb4f9f26f8
joniocomputer <56183806+joniocomputer@users.noreply.github.com>
2020-04-30 21:50:36 +0200

    Add files via upload

---
+++ aptresolver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo "[Jonio APTResolver]"
+if [ `id -u` != 0 ]; then
+   echo "E: Permission Denied" 
+   exit 1
+fi
+fileLista="/etc/apt/source.list"
+echo "Insert the name of the repo which you want to add"
+read NOMEREPO
+echo "deb http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu buster main" >> $fileLista 
+echo "deb-src http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu buster main " >> $fileLista
+echo "Insert the fingerprint of the repo"
+read CHIAVE
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv $CHIAVE
+sudo apt update
+sudo dpkg --configure -a
+sudo apt --fix-broken install
