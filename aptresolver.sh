072b649714d80d17d964799ae286f84daa81b075
joniocomputer <56183806+joniocomputer@users.noreply.github.com>
2020-04-30 18:07:18 +0200

    Update aptresolver.sh

--- aptresolver.sh
+++ aptresolver.sh
@@ -9,9 +9,9 @@ fi
 fileLista="/etc/apt/source.list"
 echo "Inserisci nome della repository da aggiungere"
 read NOMEREPO
-echo "deb http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu focal main" >> $fileLista
+echo "deb http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu buster main" >> $fileLista
 #Inserisce riga alla source list
-echo "deb-src http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu focal main " >> $fileLista #Inserisce riga alla source list
+echo "deb-src http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu buster main " >> $fileLista #Inserisce riga alla source list
 echo "Inserisci il fingerprint della repository"
 read CHIAVE
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv $CHIAVE
