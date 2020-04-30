317332c9033efa591e6182bde8dd9383810c939c
joniocomputer <56183806+joniocomputer@users.noreply.github.com>
2020-04-30 17:45:49 +0200

    Add files via upload

---
+++ aptresolver.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+echo "\e[7m[Jonio AddRepo]""\e[0m"
+if [ `id -u` != 0 ]; then
+   echo "E: Permission Denied!" 
+   exit 1
+fi
+fileLista="/etc/apt/source.list"
+echo "Inserisci nome della repository da aggiungere"
+read NOMEREPO
+echo "deb http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu focal main" >> $fileLista
+#Inserisce riga alla source list
+echo "deb-src http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu focal main " >> $fileLista #Inserisce riga alla source list
+echo "Inserisci il fingerprint della repository"
+read CHIAVE
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv $CHIAVE
+sudo apt update
+sudo dpkg --configure -a
+sudo apt --fix-broken install
