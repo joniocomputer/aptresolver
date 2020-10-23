c3bd1440a53aaae3fe97bdacd7e1b2f8b56fb130
Vincenzo Talarico <56183806+oneafter909@users.noreply.github.com>
2020-10-23 03:29:41 +0200

    Adapt packages to Ubuntu focal

--- aptresolver.sh
+++ aptresolver.sh
@@ -9,8 +9,8 @@ fi
 fileLista="/etc/apt/source.list"
 echo "Insert the name of the repo which you want to add"
 read NOMEREPO
-echo "deb http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu buster main" >> $fileLista 
-echo "deb-src http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu buster main " >> $fileLista
+echo "deb http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu focal main" >> $fileLista 
+echo "deb-src http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu focal main " >> $fileLista
 echo "Insert the fingerprint of the repo"
 read CHIAVE
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv $CHIAVE
